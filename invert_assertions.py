b59e3b89eed0e570fbebc0ab1c55d6d89201630a
JJ Garzella <jjgarzella@gmail.com>
2018-08-09 12:27:35 -0700

    Generalize main funciton in inver_assertions to handle rust.

--- invert_assertions.py
+++ invert_assertions.py
@@ -2,10 +2,10 @@ import re
 import os
 import sys
 
-def invert_single_assertion(file_string,match,replacement):
+def invert_single_assertion(file_string,match,replacement,assertion):
   """Invert an assertion by replacing the old one, using the regex match"""
 
-  repl_string = 'assert(' + match.group(1) + replacement + match.group(3) + ')'
+  repl_string = assertion + '(' + match.group(1) + replacement + match.group(3) + ')'
   result = file_string[:match.start()] + repl_string + file_string[match.end():]
   return re.sub('@expect verified', '@expect error', result)
 
@@ -22,19 +22,19 @@ def fail_filename(orig_filename,i):
   parts[-2] += fail_suffix(i)
   return '.'.join(parts)
 
-def invert_assertions(filename,original="==",replacement="!="):
+def invert_assertions(filename,original="==",replacement="!=",assertion="assert"):
   """Inverts each assertion in a file, writing each one seperately to a new file."""
 
   orig_file = ""
   with open(filename, "r") as f:
     orig_file = f.read()
   
-  regex = r'assert\((.*)(' + original + r')(.*)\)'
+  regex = assertion + r'\((.*)(' + original + r')(.*)\)'
   matches = re.finditer(regex,orig_file)
 
   i = 0
   for match in matches:
-    new_file_str = invert_single_assertion(orig_file,match,replacement)
+    new_file_str = invert_single_assertion(orig_file,match,replacement,assertion)
 
     with open(fail_filename(filename,i), "w") as f:
       f.write(new_file_str)
@@ -59,18 +59,27 @@ def main():
   folder = sys.argv[1]
   os.chdir(folder)
   for src_file in os.listdir('.'): # current directory, which we just changed into
+    if not os.path.isfile('.' + src_file):
+      continue
     parts = src_file.split('.')
     reg_name = parts[-2]
     if reg_name in reg_list:
 
-      if parts[-1] in ['f', 'f90', 'f95', 'for', 'f03']:
-        invert_assertions(src_file,"==","/=")
-        continue 
-      # just do FORTRAN hello world manually, not worth the effort
+      orig = "=="
+      repl = "!="
+      sert = "assert"
+
+      if parts[-1] in ['f', 'f90', 'f95', 'for', 'f03']: #fortran
+        repl = "/="
+      elif parts[-1] is 'rs': #rust
+        sert = "assert!"
+        continue
+
       if parts[-2] == 'hello':
-        invert_assertions(src_file,"true","false")
-      else:
-        invert_assertions(src_file)
+        orig = "true"
+        repl = "false"
+      
+      invert_assertions(src_file,orig,repl,sert)
 
 if __name__ == '__main__':
   main()
