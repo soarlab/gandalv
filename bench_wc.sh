3fe1b0deb65cfc74ddf734bff9979561edfcd619
JJ Garzella <jjgarzella@gmail.com>
2018-09-13 11:26:55 -0600

    Add useful shell scripts

---
+++ bench_wc.sh
@@ -0,0 +1,1 @@
+wc -l $(find . | grep $1 | grep -v fail)
---
+++ runall.sh
@@ -0,0 +1,9 @@
+python regtest.py --folder c --file-extension c
+python regtest.py --folder c++ --file-extension cpp
+python regtest.py --folder d --file-extension d
+python regtest.py --folder fortran --file-extension f90
+python regtest.py --folder objc --file-extension m
+python regtest.py --folder rust --file-extension rs
+python regtest.py --folder swift --file-extension swift
+python regtest.py --folder kotlin --file-extension kt
+rm *.bc *.bpl
