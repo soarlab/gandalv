0e1d014b3703df83f5e5ad62064fe23f17e9b3d7
JJ Garzella <jjgarzella@gmail.com>
2018-11-06 23:09:33 +0000

    Fix verify scripts for kotlin and swift
    Before, they would only work if called
    in the corresponding directory, now they
    should be callable from anywhere.

--- kotlin/verify.sh
+++ kotlin/verify.sh
@@ -1,11 +1,11 @@
 
+DIR=$(dirname $(realpath $1))
 FILE=$(basename $1 .kt)
-kotlinc -p bitcode -g -l reqs/assert.klib $FILE.kt reqs/smackassert.kt -o $FILE.bc
+kotlinc -p bitcode -g -l $DIR/reqs/assert.klib $DIR/$FILE.kt $DIR/reqs/smackassert.kt -o $FILE.bc
 #llvm-dis $FILE.bc 
 shift
-echo $FILE
 smack $FILE.bc \
-	reqs/cstubs.ll reqs/assert.ll reqs/Operator.cpp \
+	$DIR/reqs/cstubs.ll $DIR/reqs/assert.ll $DIR/reqs/Operator.cpp \
    	--entry-points "kfun:main(kotlin.Array<kotlin.String>)" \
 	--back-entry-points kfun_main_kotlin.Array_kotlin.String__ \
 	"$@"  
--- swift/verify.sh
+++ swift/verify.sh
@@ -1,5 +1,6 @@
+DIR=$(dirname $(realpath $1))
 FILE=$(basename $1 .swift)
-sudo swiftc -g -emit-ir -import-objc-header ./reqs/smack-Bridging-Header.h $FILE.swift > $FILE.ll
+sudo swiftc -g -emit-ir -import-objc-header $DIR/reqs/smack-Bridging-Header.h $DIR/$FILE.swift > $FILE.ll
 shift
 smack $FILE.ll "$@"
 rm $FILE.ll
